47c082f3355d78507ede8125a9eacd3569c065ab
tcgriffith <ztc.ustcer@gmail.com>
2020-11-27 15:46:26 +1000

    ¯\_(ツ)_/¯

---
+++ src/do_gremlin.sh
@@ -0,0 +1,21 @@
+
+
+casedir=$1
+
+
+caseid=$(basename $casedir)
+
+
+cd $casedir
+
+
+
+
+esl-reformat afa cmalign/$caseid.cmalign.a2m > cmalign/$caseid.cmalign.afa
+
+gremlin_cpp -alphabet rna -i cmalign/$caseid.cmalign.afa -o cmalign/$caseid.cmalign.afa.gremlincpp -gap_cutoff 0.5 -mrf_o cmalign/$caseid.cmalign.afa.mrf
+
+
+esl-reformat afa rnamrf/$caseid.rnamrf.a2m > rnamrf/$caseid.rnamrf.afa
+
+gremlin_cpp -alphabet rna -i rnamrf/$caseid.rnamrf.afa -o rnamrf/$caseid.rnamrf.afa.gremlincpp -gap_cutoff 0.5 -mrf_o rnamrf/$caseid.rnamrf.afa.mrf
